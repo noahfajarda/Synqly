7c9713761cf7e226523f98e750a694e8c7a43b39
Noah Fajarda <noahfajarda@gmail.com>
2023-11-21 02:01:13 -0800

    added content for top, bottom, left, right sidebars

--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -1,3 +1,4 @@
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,6 +9,32 @@ export default function TopBar() {
         <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
         <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
+
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          {/* will show if logged In */}
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                ></Image>
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        {/* organization switcher = create organization from clerk */}
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
+        />
+      </div>
     </nav>
   );
 }
