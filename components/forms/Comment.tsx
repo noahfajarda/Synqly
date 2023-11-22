9cd6d7a46fa4438ff69696787b2432d90d47f71c
Noah Fajarda <noahfajarda@gmail.com>
2023-11-21 23:02:27 -0800

    created comment form that links with comment action route

---
+++ components/forms/Comment.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+// UI components
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+// form validation with zod
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+// pathname and router
+import { usePathname, useRouter } from "next/navigation";
+// user validation schema
+import { CommentValidation } from "@/lib/validations/thread";
+import Image from "next/image";
+import { addCommentToThread } from "@/lib/actions/thread.actions";
+// import { createThread } from "@/lib/actions/thread.actions";
+
+interface Props {
+  threadId: string;
+  currentUserImg: string;
+  currentUserId: string;
+}
+
+export default function Comment({
+  threadId,
+  currentUserImg,
+  currentUserId,
+}: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  // form creation with form validation with zod
+  const form = useForm({
+    resolver: zodResolver(CommentValidation),
+    defaultValues: {
+      thread: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
+    // submission to add comment to thread
+    await addCommentToThread(
+      threadId,
+      values.thread,
+      JSON.parse(currentUserId),
+      pathname
+    );
+
+    form.reset();
+  };
+
+  return (
+    <Form {...form}>
+      {/* ui for commenting on thread */}
+      <form onSubmit={form.handleSubmit(onSubmit)} className="comment-form">
+        {/* render text area field input */}
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex gap-3 w-full items-center">
+              <FormLabel>
+                <Image
+                  src={currentUserImg}
+                  alt="Profile Image"
+                  width={48}
+                  height={48}
+                  className="rounded-full object-cover"
+                />
+              </FormLabel>
+              <FormControl className="border-none bg-transparent">
+                <Input
+                  type="text"
+                  placeholder="Comment..."
+                  className="text-light-1 outline-none no-focus"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="comment-form_btn">
+          Reply
+        </Button>
+      </form>
+    </Form>
+  );
+}
