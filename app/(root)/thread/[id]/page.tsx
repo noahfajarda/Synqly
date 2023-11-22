d82f95c1e2b43121e37b40302e27cfdcda5f0c07
Noah Fajarda <noahfajarda@gmail.com>
2023-11-21 23:03:07 -0800

    added comment box and comments to individual post page

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,4 +1,5 @@
 import ThreadCard from "@/components/cards/ThreadCard";
+import Comment from "@/components/forms/Comment";
 import { readOneThread } from "@/lib/actions/thread.actions";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
@@ -20,6 +21,7 @@ export default async function Page({ params }: { params: { id: string } }) {
   return (
     <section className="relative">
       <div>
+        {/* display thread card */}
         <ThreadCard
           key={thread._id}
           id={thread._id}
@@ -31,6 +33,32 @@ export default async function Page({ params }: { params: { id: string } }) {
           createdAt={thread.createdAt}
           comments={thread.children}
         />
+      </div>
+      <div className="mt-7">
+        {/* display comment area */}
+        <Comment
+          threadId={thread.id}
+          currentUserImg={userInfo.image}
+          currentUserId={JSON.stringify(userInfo._id)}
+        />
+      </div>
+
+      <div className="mt-10">
+        {thread.children.map((childItem: any) => (
+          // display thread card for each existing comment
+          <ThreadCard
+            id={childItem._id}
+            key={childItem._id}
+            currentUserId={user?.id || ""}
+            parentId={childItem.parentId}
+            content={childItem.text}
+            author={childItem.author}
+            community={childItem.community}
+            createdAt={childItem.createdAt}
+            comments={childItem.children}
+            isComment
+          />
+        ))}
       </div>
     </section>
   );
