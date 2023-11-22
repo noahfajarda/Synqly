bb3a2ff1064ea4f36320d2c42729a2834a659fb1
Noah Fajarda <noahfajarda@gmail.com>
2023-11-21 23:03:30 -0800

    added conditional for nested posts/comments

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -37,7 +37,12 @@ export default function ThreadCard({
   isComment,
 }: Props) {
   return (
-    <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+    <article
+      // conditional spacing for if card is a comment of the parent post
+      className={`flex w-full flex-col rounded-xl ${
+        isComment ? `px-0 xs:px-7` : `bg-dark-2 p-7`
+      }`}
+    >
       <div className="flex items-start justify-between">
         <div className="flex w-full flex-1 flex-row gap-4">
           <div className="flex flex-col items-center">
