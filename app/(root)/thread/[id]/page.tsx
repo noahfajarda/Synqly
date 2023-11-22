e37031541c4e909c2188763b97e7d6ed062f02be
Noah Fajarda <noahfajarda@gmail.com>
2023-11-21 18:13:31 -0800

    created singular post page retrieving data from action

---
+++ app/(root)/thread/[id]/page.tsx
@@ -0,0 +1,37 @@
+import ThreadCard from "@/components/cards/ThreadCard";
+import { readOneThread } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  if (!params.id) return null;
+
+  // check for user logged in and user info
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  // retrieve ONE post data and render card
+  const thread = await readOneThread(params.id);
+
+  return (
+    <section className="relative">
+      <div>
+        <ThreadCard
+          key={thread._id}
+          id={thread._id}
+          currentUserId={user?.id || ""}
+          parentId={thread.parentId}
+          content={thread.text}
+          author={thread.author}
+          community={thread.community}
+          createdAt={thread.createdAt}
+          comments={thread.children}
+        />
+      </div>
+    </section>
+  );
+}
