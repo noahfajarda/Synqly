3ba2e3d2b59f58d191d5667916214e449da92f9c
Noah Fajarda <noahfajarda@gmail.com>
2023-11-21 16:22:32 -0800

    created 'actions' for user model to CREATE, UPDATE, and READ ONE users

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,60 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose";
+
+interface Params {
+  userId: string;
+  username: string;
+  name: string;
+  bio: string;
+  image: string;
+  path: string;
+}
+
+export async function updateUser({
+  userId,
+  username,
+  name,
+  bio,
+  image,
+  path,
+}: Params): Promise<void> {
+  connectToDB();
+
+  try {
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        username: username.toLowerCase(),
+        name,
+        bio,
+        image,
+        onboarded: true,
+      },
+      { upsert: true } // upsert: insert OR update (if already exists)
+    );
+
+    if (path === "/profile/edit") {
+      // revalidate data mannually using path without relying on cache
+      revalidatePath(path);
+    }
+  } catch (error: any) {
+    throw new Error(`Failed to create/update user: ${error.message}`);
+  }
+}
+
+export async function fetchUser(userId: string) {
+  try {
+    connectToDB();
+
+    return await User.findOne({ id: userId });
+    // .populate({
+    //   path: 'communities',
+    //   model: Community
+    // })
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user: ${error.message}`);
+  }
+}
