2b6beffa660c3d9e7371579a16f1c6f1f3747c0c
Noah Fajarda <noahfajarda@gmail.com>
2023-11-24 23:06:26 -0800

    added suggested users and hover effect

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -26,7 +26,9 @@ export default function LeftSidebar() {
             <Link
               href={link.route}
               key={link.label}
-              className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
+              className={`leftsidebar_link ${
+                isActive && "bg-primary-500"
+              } hover:bg-violet-400 transition-all`}
             >
               <Image
                 src={link.imgURL}
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,4 +1,35 @@
-export default function RightSidebar() {
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import UserCard from "@/components/cards/UserCard";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function RightSidebar() {
+  // check for current user and info
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("onboarding");
+
+  // fetch users with action
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
+  // get at most 3 random users
+  if (result.users.length > 3) {
+    const indices = new Set();
+
+    while (indices.size < 3) {
+      const randomIndex = Math.floor(Math.random() * result.users.length);
+      indices.add(randomIndex);
+    }
+    console.log(Array.from(indices));
+  }
+
   return (
     <section className="custom-scrollbar rightsidebar">
       <div className="flex flex-1 flex-col justify-start">
@@ -8,6 +39,20 @@ export default function RightSidebar() {
       </div>
       <div className="flex flex-1 flex-col justify-start">
         <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
+        <div className="p-4">
+          {result.users.map((person, idx) => (
+            <div key={idx} className="py-3">
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType="User"
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
